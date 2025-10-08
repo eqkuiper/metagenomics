8d9cc26b0ac362bba04ca418f1c3fcaa1c4bcec8
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-10-08 12:39:39 -0500

    polish ribotagger_biom.sh

--- code/ribotagger_biom.sh
+++ code/ribotagger_biom.sh
@@ -19,11 +19,39 @@ ribotagger_dir="/projects/p32449/goop_stirrers/ribotagger-master/ribotagger" # p
 experiment_name="mammoth_metagenomes" # project name to be used in naming output files
 #################
 
+# error checks
+if [ ! -d "$in_dir" ]; then
+    echo "ERROR: Input directory $in_dir does not exist." >&2
+    exit 1
+fi
+
+if [ ! -d "$ribotagger_dir" ]; then
+    echo "ERROR: Ribotagger directory $ribotagger_dir does not exist." >&2
+    exit 1
+fi
+
+if [ ! -f "${ribotagger_dir}/biom.pl" ]; then
+    echo "ERROR: biom.pl not found in $ribotagger_dir." >&2
+    exit 1
+fi
+
+shopt -s nullglob
+files=("$in_dir"/*.${region})
+if [ ${#files[@]} -eq 0 ]; then
+    echo "ERROR: No input files found in $in_dir with extension .${region}" >&2
+    exit 1
+fi
+shopt -u nullglob
+
 # make output directory if it doesn't already exist
 mkdir -p "$out_dir" 
 
 # load perl module
 module load perl
 
+echo "Running biom.pl from the ribotagger package for $experiment_name"
+
 # run biom.pl from the ribotagger package
 "${ribotagger_dir}/biom.pl" -r "$region" -i $in_dir/*.${region} -o "${out_dir}/${experiment_name}"
+
+echo "Finished combining files $in_dir. Outputs are in $out_dir. Enjoy your $(day +%A)." 
