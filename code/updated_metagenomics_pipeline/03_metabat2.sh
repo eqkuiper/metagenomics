29f32b5d58e1cb9eca9ca04b47b12485a3659131
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-10-10 10:40:06 -0500

    update LD lib path and fix output dir

--- code/updated_metagenomics_pipeline/03_metabat2.sh
+++ code/updated_metagenomics_pipeline/03_metabat2.sh
@@ -19,32 +19,33 @@ min_contig_length=(3000 2500 2000 1500) # should be >= 1500
 
 # define output
 out_dir=${parent_dir}/03_metabat2-scaffolds-c_${min_contig_length[$SLURM_ARRAY_TASK_ID]}
-mkdir -p $out_dir 
+mkdir -p "$out_dir"
 
 # load metabat2 conda environment
 module load python-miniconda3
 source activate /projects/p31618/software/metabat2-2.18
+export LD_LIBRARY_PATH=/projects/p31618/software/metabat2-2.18/lib:$LD_LIBRARY_PATH
 
 # bin contigs with metabat2 for each subdirectory:
 for dir in "$spades_dir"/*/; do
 
-contigs_dir="${dir}"
+sample=$(basename $dir)
 
 echo "Binning from:"
-echo "${contigs_dir}"
+echo "$dir"
 
-mkdir -p $out_dir/$contigs_dir
+mkdir -p $out_dir/$sample
 
 SECONDS=0
 metabat \
 -i $contigs_dir/scaffolds.fasta \
 -m ${min_contig_length[$SLURM_ARRAY_TASK_ID]} \
--o $out_dir/$contigs_dir/$contigs_dir \
--t $slurm_ntasks \
+-o $out_dir/$sample/${sample}_bin \
+-t $SLURM_NTASKS \
 --unbinned
 
 echo "[$SECONDS seconds] required to bin MAGs."
-echo "Saved to "$out_dir/$contigs_dir"."
+echo "Saved to "$out_dir/$sample"."
 echo "Have a wonderful $(date +%A), and don't your dreams be dreams."
 
 done
