1cbe18357eae16dbe2b5a073ace514a77b00cacb
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-11-14 10:34:58 -0600

    update bin refinement step

--- code/updated_metagenomics_pipeline/06_bin_refinement.sh
+++ code/updated_metagenomics_pipeline/06_bin_refinement.sh
@@ -4,32 +4,27 @@
 #SBATCH -t 48:00:00
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --array=0-20 # change to number of metagenomes
+#SBATCH --array=0-2 # change to number of metagenomes
 #SBATCH --mem-per-cpu=20G
 #SBATCH --job-name=bin_refinement-50compl-50contam
 #SBATCH --mail-user=esmee@u.northwestern.edu # change to your email
 #SBATCH --mail-type=START,FAIL,END
 #SBATCH --output=%A_%a-%x.out
 
-### USER INPUTS
-# define parent genome directory and trimmed reads folder
-parent_dir=/projects/p32449/maca_mags_metabolic/data/2025-10-07_maca_metaG 
-out_dir=/projects/p32449/maca_mags_metabolic/data/2025-10-07_maca_metaG  # output directory
-reads_dir=${parent_dir}/01_trimmomatic_out # trimmed reads folder
-sample_list=/projects/p32449/maca_mags_metabolic/data/mags_to_annotate_assemblies.txt # list of samples
-###############
-
-# list of samples from which to bin:
-IFS=$'\n' read -d '' -r -a input_args < /path/to/list/of/samples
-metagenome=${input_args[$SLURM_ARRAY_TASK_ID]}
-
 #### user inputs ####
-# define parent genome directory and trimmed reads folder
-parent_dir=/projects/p30996/mammoth/metagenomes
-initial_bins=${parent_dir}/metaWRAP-initial-bins/${metagenome}
-output_dir=${parent_dir}/metaWRAP-refined-bins-50compl-50contam/${metagenome}
+## note that you may need to chance the structure of the actual metawrap command (at the bottom of this file)
+## depending on the structure of your bin directories
+sample_list=/projects/p32449/maca_mags_metabolic/data/mags_to_annotate_test.txt
+parent_dir=/projects/p32449/maca_mags_metabolic/data
+bins_a_dir=/scratch/jhr1326/2025-10-21_COMEBin
+bins_b_dir=${parent_dir}/2025-10-07_maca_metaG/03_metabat2-scaffolds-c_1500
+bins_c_dir=/scratch/jhr1326/metadecoder_results
+output_dir=${parent_dir}/2025-10-07_maca_metaG/metaWRAP-refined-bins-50compl-50contam
 ####             ####
 
+# list of samples from which to bin:
+IFS=$'\n' read -d '' -r -a input_args < ${sample_list}
+metagenome=${input_args[$SLURM_ARRAY_TASK_ID]}
 
 module purge all
 module load checkm
@@ -37,14 +32,14 @@ module load mamba
 source activate /projects/p31618/software/metawrap
 PATH=/projects/p31618/software/metaWRAP/bin:$PATH
 
-mkdir -p ${parent_dir}/metaWRAP-refined-bins-50compl-50contam
+mkdir -p "${output_dir}/${metagenome}"
 
 metawrap bin_refinement \
-  -o ${output_dir} \
+  -o ${output_dir}/${metagenome} \
   -t $SLURM_NTASKS \
-  -A ${initial_bins}/metabat2_bins/ \
-  -B ${initial_bins}/maxbin2_bins/ \
-  -C ${initial_bins}/concoct_bins/ \
+  -A ${bins_a_dir}/${metagenome}/comebin_res/comebin_res_bins \
+  -B ${bins_b_dir}/${metagenome} \
+  -C ${bins_c_dir}/${metagenome} \
   -c 50 -x 50
 
 # back up results to scratch space
