176079d419560414af1d842bd35e3b8ada0263ad
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-11-14 11:35:31 -0600

    udpate inputs

--- code/mags_nmds/01_coverm.sh
+++ code/mags_nmds/01_coverm.sh
@@ -6,27 +6,45 @@
 #SBATCH -n 16
 #SBATCH --mem=20G
 #SBATCH --job-name=coverm
-#SBATCH --mail-user=esmee@u.northwestern.edu # change to your email
+#SBATCH --mail-user=esmee@u.northwestern.edu
 #SBATCH --mail-type=START,FAIL,END
 #SBATCH --output=%A_%a-%x.out
 
 # user inputs
 bams_dir=/scratch/jhr1326/2025-10-20_02.5_align
-mags_dir=/projects/p32449/maca_mags_metabolic/2025-09-16_metagenomes
+mags_dir=/projects/p32449/maca_mags_metabolic/data/2025-09-16_metagenomes
+tmp_dir=/scratch/jhr1326/maca_mags_metabolic_tmp
 out_dir=/projects/p32449/maca_mags_metabolic/2025-11-14_coverm
 # -----------
 
 mkdir -p $out_dir
+mkdir -p $tmp_dir
 
-# load coverm
+# copy and rename MAGs to tmp dir with underscores
+cd $mags_dir
+for f in *.orig.fasta *.strict.fasta *.permissive.fasta; do
+    # convert filename suffix
+    newname="${f/.orig.fasta/_orig.fna}"
+    newname="${newname/.strict.fasta/_strict.fna}"
+    newname="${newname/.permissive.fasta/_permissive.fna}"
+
+    # copy to tmp
+    cp "$f" "$tmp_dir/$newname"
+
+    # make contig headers unique by prepending MAG name
+    mag_base=$(basename "$newname" .fna)
+    sed -i "s/^>/>${mag_base}_/g" "$tmp_dir/$newname"
+done
+
+# load coverm environment
 module load python-miniconda3
-eval "$(conda shell.bash hook)" 
+eval "$(conda shell.bash hook)"
 conda activate /projects/p32449/goop_stirrers/miniconda3/envs/coverm
 
-# run coverm
+# run coverm using the tmp MAGs
 coverm genome \
     -b ${bams_dir}/*/*_sorted.bam \
     -m relative_abundance \
-    --genome-fasta-directory ${mags_dir} \
+    --genome-fasta-directory $tmp_dir \
     --threads 16 \
     > ${out_dir}/coverm_relative_abundance.tsv
