09c41c45e7cad132421a2ec5a1fc2c32eef6d5b0
eqkuiper <esmeekuiper2028@u.northwestern.edu>
2025-11-14 10:05:38 -0600

    fix typos

--- code/updated_binners/semibin.sh
+++ code/updated_binners/semibin.sh
@@ -7,7 +7,7 @@
 #SBATCH -n 4
 #SBATCH --mem=50G
 #SBATCH --array=0-20                        # <-- adjust based on sample count
-#SBATCH --job-name=semibin_array
+#SBATCH --job-name=semibin_array_take_2
 #SBATCH --mail-user=esmee@u.northwestern.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=%A_%a_semibin.out  # one log per array job
@@ -24,10 +24,10 @@ bam_dir="/scratch/jhr1326/2025-10-20_02.5_align"
 fasta_dir="/scratch/jhr1326/02_assembled-spades_10Nov2025"
 
 # --- Output root directory (one subdir per metagenome will be made)
-out_root="/scratch/jhr1326/semibin_results"
+out_root="/scratch/jhr1326/semibin_results_gpu"
 
 # --- Conda environment for MetaDecoder
-semibin_env="/projects/p31618/software/SemiBin-2.2.0"
+semibin_env="/projects/p32449/goop_stirrers/miniconda3/envs/SemiBin"
 
 ################################################################################
 #                       PIPELINE EXECUTION (DO NOT EDIT BELOW)
